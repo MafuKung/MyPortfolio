c0bdee52120b5e291479321ba3b4b006b6fee777
MafuKung <siramet603@gmail.com>
2024-10-03 12:51:45 +0700

    fix chatbot card

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -71,7 +71,7 @@ function Projects() {
               isBlog={false}
               title=" A Chatbot for Learning Space Help Desk for OCSC Learning Space (In progress)"
               description="A Chatbot for Learning Space Help Desk for OCSC Learning Space (In progress) A senior project about integrates Generative AI to assist in answering user question about a OCSC Learning Space website in place of the support team."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              //ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>      
